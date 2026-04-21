7409fba64e81cfc3ea725dd1efbd1886c009d100
Ernesto-Tz <56386400+Ernesto-Tz@users.noreply.github.com>
2026-04-21 17:20:39 +0200

    refactor: remove unused OnceUI configuration objects

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -44,108 +44,10 @@ const font = {
   code: monoFont,
 };
 
-const style = {
-  theme: "dark", // theme is not necessary when using ThemeProvider (default)
-  neutral: "sand", // sand | gray | slate
-  brand: "yellow", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: "orange", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  solid: "contrast", // color | contrast
-  solidStyle: "flat", // flat | plastic
-  border: "conservative", // rounded | playful | conservative
-  surface: "translucent", // filled | translucent
-  transition: "all", // all | micro | macro
-  scaling: "100" // 90 | 95 | 100 | 105 | 110
-};
-
-const effects = {
-  mask: {
-    cursor: false,
-    x: 50,
-    y: 0,
-    radius: 100,
-  },
-  gradient: {
-    display: true,
-    opacity: 25,
-    x: 10,
-    y: 85,
-    width: 55,
-    height: 45,
-    tilt: 0,
-    colorStart: "brand-background-strong",
-    colorEnd: "page-background",
-  },
-  dots: {
-    display: false,
-    opacity: 40,
-    size: "2",
-    color: "brand-background-strong",
-  },
-  grid: {
-    display: false,
-    opacity: 100,
-    color: "neutral-alpha-medium",
-    width: "0.25rem",
-    height: "0.25rem",
-  },
-  lines: {
-    display: false,
-    opacity: 15,
-    color: "neutral-alpha-weak",
-    size: "64",
-    thickness: 1,
-    angle: 45,
-  },
-};
-
 const display = {
   location: true,
   time: true,
-  themeSwitcher: true
-};
-
-const mailchimp = {
-  action: "https://url/subscribe/post?parameters",
-  effects: {
-    mask: {
-      cursor: true,
-      x: 50,
-      y: 0,
-      radius: 100,
-    },
-    gradient: {
-      display: true,
-      opacity: 90,
-      x: 50,
-      y: 0,
-      width: 50,
-      height: 50,
-      tilt: 0,
-      colorStart: "accent-background-strong",
-      colorEnd: "static-transparent",
-    },
-    dots: {
-      display: true,
-      opacity: 20,
-      size: "2",
-      color: "brand-on-background-weak",
-    },
-    grid: {
-      display: false,
-      opacity: 100,
-      color: "neutral-alpha-medium",
-      width: "0.25rem",
-      height: "0.25rem",
-    },
-    lines: {
-      display: false,
-      opacity: 100,
-      color: "neutral-alpha-medium",
-      size: "16",
-      thickness: 1,
-      angle: 90,
-    },
-  },
+  themeSwitcher: true,
 };
 
 const nav = {
@@ -161,4 +63,4 @@ const site = {
   image: "/images/og/home.jpg",
 };
 
-export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL, font, nav, site };
+export { routes, protectedRoutes, display, baseURL, font, nav, site };
